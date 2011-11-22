83b1a69bcb9d3132e7478ae2cc383cbae4920f31
jinntrance <jinntrance@5a745181-bd65-8625-f5e4-348bc6d42813>
2011-11-22 02:37:16 +0000

    fix the isssue of not responding for the "Douban it"

--- trunk/web/js/search.js
+++ trunk/web/js/search.js
@@ -12,5 +12,5 @@ function search(data,tab){
 	'url':search_url+data.selectionText,/*选取文字加url完成*/
 	'selected':false/*false为不切换至打开页面*/
 	}
-	chrome.tabs.create(newTabl);
+	chrome.tabs.create(newTab);
 }
