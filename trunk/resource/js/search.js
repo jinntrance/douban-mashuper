abf5009a4399633e8d52400655290fd84c4b26e6
jinntrance <jinntrance@5a745181-bd65-8625-f5e4-348bc6d42813>
2012-10-04 07:18:46 +0000

    fix the bug after douban UI updated.

--- trunk/resource/js/search.js
+++ trunk/resource/js/search.js
@@ -1,16 +1,43 @@
-﻿var search_url='http://book.douban.com/subject_search?cat=1001&search_text='
-var properties={
-	'type':'normal',
-	'title':'豆瓣一下下',
-	'onclick':search,
-	'contexts':['selection']
-}
-chrome.contextMenus.create(properties, function(){})
+﻿var book_url='http://book.douban.com/subject_search?search_text='
+var movie_url='http://movie.douban.com/subject_search?search_text='
+var music_url='http://music.douban.com/subject_search?search_text='
+var parent =chrome.contextMenus.create(
+    {'type':'normal','title':'豆瓣一下下','contexts':['selection']}, function(){});
+// Create a parent item and two children.
+var book = chrome.contextMenus.create(
+    {"title": "搜图书", "parentId": parent, "onclick": searchBook,'contexts':['selection']}, function(){});
+var movie = chrome.contextMenus.create(
+    {"title": "搜电影", "parentId": parent, "onclick": searchMovie,'contexts':['selection']}, function(){});
+var music = chrome.contextMenus.create(
+    {"title": "搜音乐", "parentId": parent, "onclick": searchMusic,'contexts':['selection']}, function(){});
+
 
 function search(data,tab){
 	var newTab={
 	'url':search_url+data.selectionText,/*选取文字加url完成*/
 	'selected':false/*false为不切换至打开页面*/
-	}
+	};
 	chrome.tabs.create(newTab);
+}
+function searchBook(data,tab){
+    chrome.tabs.create(formURL("book",data.selectionText));
+}
+function searchMovie(data,tab){
+    chrome.tabs.create(formURL("movie",data.selectionText));
+}
+function searchMusic(data,tab){
+    chrome.tabs.create(formURL("music",data.selectionText));
+}
+function formURL(type,selectedText){
+    var newURL='http://book.douban.com/subject_search?search_text='+selectedText;/*选取文字加url完成*/
+    switch (type){
+       case "book":
+       case "movie":
+       case "music":
+             newURL="http://"+type+".douban.com/subject_search?search_text="+selectedText;
+    }
+    return {
+        'url':newURL,
+        'selected':false/*false为不切换至打开页面*/
+    };
 }
