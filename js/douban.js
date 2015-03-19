f09e8abff66db2e8341bc5cc57e8257d9a3f14cd
Joseph J.C. Tang <jinntrance@gmail.com>
2015-03-20 00:46:43 +0800

    add collections of book info

--- js/douban.js
+++ js/douban.js
@@ -1,9 +1,11 @@
 var count=100
 var url="https://api.douban.com/v2/book/user/jinntrance/collections?status=wish&count="+count+"&start="
 titles=[]
+books=[]
 for(var i = 0; i<8;i++){
   full_url = url + i*count
   $.getJSON(full_url, funciton(json){
+    books.concat(json.collections)
     titles.concat(json.collections.map(function(b){
       return b.book.title
     }))
