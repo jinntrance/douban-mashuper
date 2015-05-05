5f952158729ec0b53631e04038dff50f7b774014
Joseph J.C. Tang <jinntrance@gmail.com>
2015-05-06 02:22:36 +0800

    remove unused files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-if ('book.douban.com' == document.domain)
-    handleIt();
-else if ('webpac.uestc.edu.cn' == document.domain || '222.197.164.247'==document.domain) {
-    var d = $(bk_displayed_selector);
-    if (null != d && 0 < d.length) {
-        addListener();
-        displayReview();
-    }
-}
