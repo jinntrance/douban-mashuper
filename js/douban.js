d901815534d79c2cdc0b487d13fd21d48f697061
Joseph J.C. Tang <jinntrance@gmail.com>
2015-05-20 20:26:07 +0800

    douban wish support

--- js/douban.js
+++ js/douban.js
@@ -2,12 +2,27 @@ var count=100
 var url="https://api.douban.com/v2/book/user/jinntrance/collections?status=wish&count="+count+"&start="
 titles=[]
 books=[]
-for(var i = 0; i<8;i++){
+limit = 1
+books_in_lib = []
+for(var i = 0; i<limit;i++){
   full_url = url + i*count
-  $.getJSON(full_url, funciton(json){
-    books.concat(json.collections)
-    titles.concat(json.collections.map(function(b){
-      return b.book.title
-    }))
+  $.getJSON(full_url, function(json){
+    limit = Math.ceil(1.0 * json.total / count)
+    limit = 1
+    books.concat(json.collections);
+    bk = json.collections.map(function(b){
+      title = b.book.title;
+      fullUrl = search_prefix + title
+      get_html(fullUrl, function(data) {
+        var m = $(data).find(bk_displayed_selector);//有結果顯示 
+          if (null != m && 0 < m.length) {
+            books_in_lib.push(title)
+          } 
+      });
+      return title
+    })    
+    titles = titles.concat(bk)
+    console.info(books_in_lib.join(" "))
   })
 }
+
