05c87be694687936e5737a73878131211cbea9fc
Joseph J.C. Tang <jinntrance@gmail.com>
2016-04-07 21:40:09 +0800

    add new selector

--- js/public.js
+++ js/public.js
@@ -1,5 +1,5 @@
-var bk_displayed_selector='#books li'; // 改页面解析，判断是否有藏书的selector 
-var search_prefix = "http://mit.sankuai.com/book/books?keyword="; // 搜索url，只需要加搜索书名
+var bk_displayed_selector='cbooks li'; // 改页面解析，判断是否有藏书的selector 
+var search_prefix = "http://book.sankuai.com/#/search?q="; // 搜索url，只需要加搜索书名
 lib_prefix="美团图书馆" //藏书图书馆名字
 
 
