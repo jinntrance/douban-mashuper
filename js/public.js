24cd62762edec09b0fbb4fa616399433bce49876
Joseph J.C. Tang <jinntrance@gmail.com>
2016-04-08 22:49:14 +0800

    fix authorization

--- js/public.js
+++ js/public.js
@@ -1,5 +1,6 @@
 var bk_displayed_selector='$.0.title'; // 改页面解析，判断是否有藏书的selector 
 var search_prefix = "//api-mit.sankuai.com/library/books?q="; // 搜索url，只需要加搜索书名
+var display_prefix = '//book.sankuai.com/#/search?q='
 lib_prefix="美团图书馆" //藏书图书馆名字
 
 
@@ -14,6 +15,7 @@ function get_html(url,callback){
     beforeSend: function (request)
     {
         request.setRequestHeader("Cache-Control", "max-age=0");
+        request.setRequestHeader("authorization", "");
         request.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
     },
     xhr: function() {
--- js/query.js
+++ js/query.js
@@ -1,10 +1,12 @@
 var list_id='r_list'; //右面列表div's id
 var fullUrl;
+var display_url;
 
 function handleIt() {
     var book = parse();
     var ISBN = book.isbn;
     fullUrl = search_prefix + book.title;
+    display_url= display_prefix + book.title;
     get_html(fullUrl, function(data) {
         var m = undefined;
         if (bk_displayed_selector[0] == '$') {
@@ -32,7 +34,7 @@ function insertSidebar(hint) {
     $('div#buyinfo').before('<div id="resourse" class="gray_ad"></div>');
     var lib="图书资源"
     if (window.lib_prefix && lib_prefix.length>1) lib = lib_prefix;
-    $('#resourse').append("<h2>"+lib+"&nbsp;<a target='_blank' href='"+fullUrl+"' >"+hint+"</a>")
+    $('#resourse').append("<h2>"+lib+"&nbsp;<a target='_blank' href='"+display_url+"' >"+hint+"</a>")
 }
 
 /*
